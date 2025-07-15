16d7a1d0620d92bbc14e03888a15f73c8712289d
Y3llow45 <hristovborislav45@gmail.com>
2025-07-15 14:26:13 +0300

    Create TodoController.java

---
+++ todo-managment/src/main/java/com/example/todo_managment/controller/TodoController.java
@@ -0,0 +1,21 @@
+package com.example.todo_managment.controller;
+
+import com.example.todo_managment.dto.TodoDto;
+import com.example.todo_managment.service.TodoService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/todos")
+@AllArgsConstructor
+public class TodoController {
+    private TodoService todoService;
+
+    @PostMapping
+    public ResponseEntity<TodoDto> createTodo(@RequestBody TodoDto todoDto) {
+        TodoDto createdTodo = todoService.createTodo(todoDto);
+        return new ResponseEntity<>(createdTodo, HttpStatus.CREATED);
+    }
+}
