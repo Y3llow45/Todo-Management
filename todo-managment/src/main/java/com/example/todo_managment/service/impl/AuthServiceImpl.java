c2acf6a5331e8910bc20452075561fc0aae22dc9
Y3llow45 <hristovborislav45@gmail.com>
2025-08-03 16:53:50 +0300

    Create AuthServiceImpl.java

---
+++ todo-managment/src/main/java/com/example/todo_managment/service/impl/AuthServiceImpl.java
@@ -0,0 +1,50 @@
+package com.example.todo_managment.service.impl;
+
+import com.example.todo_managment.dto.RegisterDto;
+import com.example.todo_managment.entity.Role;
+import com.example.todo_managment.entity.User;
+import com.example.todo_managment.exception.TodoApiException;
+import com.example.todo_managment.repository.RoleRepository;
+import com.example.todo_managment.repository.UserRepository;
+import com.example.todo_managment.service.AuthService;
+import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@AllArgsConstructor
+public class AuthServiceImpl implements AuthService {
+    private UserRepository userRepository;
+    private RoleRepository roleRepository;
+    private PasswordEncoder passwordEncoder;
+
+    @Override
+    public String register(RegisterDto registerDto) {
+
+        if (registerDto.getName() == null || registerDto.getUsername() == null ||
+            registerDto.getEmail() == null || registerDto.getPassword() == null) {
+            throw new TodoApiException(HttpStatus.BAD_REQUEST, "All fields are required");
+        }
+        if(userRepository.existsByUsername(registerDto.getUsername()) || userRepository.existsByEmail(registerDto.getEmail())) {
+            throw new TodoApiException(HttpStatus.CONFLICT, "Username or email already exists");
+        }
+
+        User user = new User();
+        user.setName(registerDto.getName());
+        user.setUsername(registerDto.getUsername());
+        user.setEmail(registerDto.getEmail());
+        user.setPassword(passwordEncoder.encode(registerDto.getPassword()));
+
+        Set<Role> roles = new HashSet<>();
+        Role userRole = roleRepository.findByName("ROLE_USER");
+        roles.add(userRole);
+        user.setRoles(roles);
+
+        userRepository.save(user);
+        return "User registered successfully";
+    }
+}
